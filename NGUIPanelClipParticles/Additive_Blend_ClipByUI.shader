6d89205cb76be71fdb117b42527b069cb380fbbe
hzy125060691 <huangzhenyu@cyou-inc.com>
2016-11-18 19:30:45 +0800

    NGUIPanelClipParticles
    NGUIPanelClipParticles

---
+++ NGUIPanelClipParticles/Additive_Blend_ClipByUI.shader
@@ -0,0 +1,109 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Hidden/Additive_Blend_ClipByUI"
+{
+	Properties
+	{
+		_TintColor("Tint Color", Color) = (0.5,0.5,0.5,0.5)
+		_MainTex("Particle Texture", 2D) = "white" {}
+	_PanelSizeX("_PanelSizeX", Range(0.000000,300.000000)) = 0.000000
+		_PanelSizeY("_PanelSizeY", Range(0.000000,300.000000)) = 0.000000
+		_PanelCenterAndSharpness("_PanelCenterAndSharpness", Vector) = (0,0,0,0)
+	}
+
+		Category
+	{
+		Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent" }
+		Blend SrcAlpha OneMinusSrcAlpha
+		AlphaTest Greater .01
+		ColorMask RGB
+		Cull Off
+		Lighting Off
+		ZWrite Off
+		Fog{ Color(0,0,0,0) }
+
+		SubShader
+	{
+		Pass
+	{
+		CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+#pragma multi_compile_particles
+
+#include "UnityCG.cginc"
+
+		sampler2D _MainTex;
+	fixed4 _TintColor;
+	float4 _PanelCenterAndSharpness;
+	float _PanelSizeX;
+	float _PanelSizeY;
+
+
+
+	struct appdata
+	{
+		float4 vertex : POSITION;
+		fixed4 color : COLOR;
+		float2 uv : TEXCOORD0;
+	};
+
+	struct v2f
+	{
+		float4 vertex : SV_POSITION;
+		fixed4 color : COLOR;
+		float2 uv : TEXCOORD0;
+		float2 posInPanel : TEXCOORD1;
+		//float2 worldPos : TEXCOORD2;
+	};
+
+	float4 _MainTex_ST;
+
+	v2f vert(appdata v)
+	{
+		v2f o;
+
+		//o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+		//o.color = v.color;
+		//o.uv = v.uv;
+		//o.worldPos = v.vertex.xy * _ClipRange0.zw + _ClipRange0.xy;
+
+
+		o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+		o.color = v.color;
+		o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+
+		float2 clipSpace = o.vertex.xy / o.vertex.w;
+
+		// Normalize clip space  
+
+		o.posInPanel = (clipSpace.xy + 1) * 0.5;
+
+		// Adjust for panel offset  
+		o.posInPanel.x -= _PanelCenterAndSharpness.x;
+		o.posInPanel.y -= _PanelCenterAndSharpness.y;
+
+		// Adjust for panel size  
+		o.posInPanel.x *= (2 / _PanelSizeX);
+		o.posInPanel.y *= (2 / _PanelSizeY);
+
+		return o;
+	}
+
+	half4 frag(v2f IN) : COLOR
+	{
+		float2 k = float2(_PanelCenterAndSharpness.z, _PanelCenterAndSharpness.w) *0.5;
+		// Softness factor  
+		float2 factor = (float2(1.0, 1.0) - abs(IN.posInPanel)) * _PanelCenterAndSharpness.zw *0.5;
+		//  (float2(1, 1) - abs(vx’, vy’)) * (pw’ / sx, ph’ / sy) 
+		//                                    pw’ = 0.5 * pw, ph’ = 0.5 * ph
+		// Sample the texture  
+		half4 col = tex2D(_MainTex, IN.uv) * IN.color;
+		col.a *= clamp(min(factor.x, factor.y), 0.0, 1.0);
+		return col;
+	}
+		ENDCG
+	}
+	}
+	}
+}
---
+++ NGUIPanelClipParticles/UIParticleClip.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIParticleClip : MonoBehaviour
+{
+	public UIPanel m_panel;
+	public string need_Change_Shader_Additive = "Particles/Additive";
+	public string need_Change_Shader_Blend_Additive = "Particles/Alpha Blended";
+	public Shader shader_Additive_ClipByUI;
+	public Shader shader_Additive_Blend_ClipByUI;
+	[HideInInspector]
+	public Camera uiCamera;
+
+	int m_panelSizeXProperty;
+	int m_panelSizeYProperty;
+	int m_panelCenterAndSharpnessProperty;
+
+	void Start()
+	{
+		if(shader_Additive_ClipByUI == null || !shader_Additive_ClipByUI.isSupported ||
+			shader_Additive_Blend_ClipByUI == null || !shader_Additive_Blend_ClipByUI.isSupported)
+		{
+			gameObject.SetActive(false);
+			Debug.LogError("replace shader is null");
+			return;
+		}
+		if (m_panel == null)
+		{
+			gameObject.SetActive(false);
+			Debug.LogError("m_panel is null");
+			return;
+		}
+		uiCamera = UIManager.Instance.UnqueUICamera;
+		if(uiCamera == null)
+		{
+			gameObject.SetActive(false);
+			Debug.LogError("uiCamera is null");
+			return;
+		}
+
+		m_panelSizeXProperty = Shader.PropertyToID("_PanelSizeX");
+		m_panelSizeYProperty = Shader.PropertyToID("_PanelSizeY");
+		m_panelCenterAndSharpnessProperty = Shader.PropertyToID("_PanelCenterAndSharpness");
+
+		UpdateClip(m_panel);
+		m_panel.onClipMove += UpdateClip;
+	}
+	void UpdateClip(UIPanel panel)
+	{
+		if (panel && panel.hasClipping)
+		{
+			var soft = panel.clipSoftness;
+			var sharpness = new Vector2(1000.0f, 1000.0f);
+
+			if (soft.x > 0f)
+			{
+				sharpness.x = panel.baseClipRegion.z / soft.x;
+			}
+			if (soft.y > 0f)
+			{
+				sharpness.y = panel.baseClipRegion.w / soft.y;
+			}
+
+			Vector4 panelCenterAndSharpness;
+
+			//var uiCamera = NGUIUtil.GetCamera();
+			Debug.Assert(uiCamera != null);
+			var panelWorldCorners = m_panel.worldCorners;
+			var leftBottom = uiCamera.WorldToViewportPoint(panelWorldCorners[0]);
+			var topRight = uiCamera.WorldToViewportPoint(panelWorldCorners[2]);
+			var center = Vector3.Lerp(leftBottom, topRight, 0.5f);
+
+			panelCenterAndSharpness.x = center.x;
+			panelCenterAndSharpness.y = center.y;
+			panelCenterAndSharpness.z = sharpness.x;
+			panelCenterAndSharpness.w = sharpness.y;
+
+			// Set shader properties
+			var pss = panel.GetComponentsInChildren<ParticleSystem>();
+			foreach (var ps in pss)
+			{
+				var render = ps.GetComponent<Renderer>();
+				if(render == null)
+				{
+					continue;
+				}
+				if(render.material != null)
+				{
+					//Shader.FindObjectOfType()
+					if(render.material.shader.name.Equals(need_Change_Shader_Additive))
+					{
+						render.material.shader = shader_Additive_ClipByUI;
+					}
+					else if(render.material.shader.name.Equals(need_Change_Shader_Blend_Additive))
+					{
+						render.material.shader = shader_Additive_Blend_ClipByUI;
+					}
+					else
+					{
+						continue;
+					}
+					
+					render.material.SetFloat(m_panelSizeXProperty, topRight.x - leftBottom.x);
+					render.material.SetFloat(m_panelSizeYProperty, topRight.y - leftBottom.y);
+					render.material.SetVector(m_panelCenterAndSharpnessProperty, panelCenterAndSharpness);
+				}
+			}
+		}
+	}
+
+}
