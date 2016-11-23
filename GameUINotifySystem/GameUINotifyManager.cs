09cdc6313d380a49fab7ee05e3ebbfb48511b2dc
hzy125060691 <huangzhenyu@cyou-inc.com>
2016-11-23 14:30:04 +0800

    GameUINotify
    GameUINotify

---
+++ GameUINotifySystem/GameUINotifyManager.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zz.GameUINotifySystem
+{
+	public abstract class IGameUINotifyEvent
+	{
+		public abstract bool CheckStatus ();
+	}
+	
+	public enum GameUINotifyType
+	{
+		None = 0,
+		Count,
+	}
+	
+	public class GameUINotifyManager
+	{
+		
+		private static GameUINotifyManager instance;
+		
+		public static GameUINotifyManager  Instance
+		{
+			get
+			{
+				if (null == instance)
+				{
+					instance = new GameUINotifyManager();
+				}
+				return instance;
+			}
+		}
+
+		private bool[] mStatusArr = new bool[ 0 ];
+		private Dictionary<int, List<System.Action>>  mChangeListeners = new Dictionary<int, List<System.Action>> ();
+		
+		private GameUINotifyManager ()
+		{
+			mStatusArr = new bool[ (int)GameUINotifyType.Count ];
+			setStatusAllOff ();
+		}
+
+		public void Clear ()
+		{
+			setStatusAllOff ();
+			mChangeListeners.Clear ();
+		}
+		
+		private void setStatusAllOff ()
+		{
+			if (mStatusArr != null) {
+				for (int i=0; i< mStatusArr.Length; ++i) 
+					mStatusArr [i] = false;
+			}
+		}
+		
+		public bool IsNeedNotify (int index)
+		{
+			if (index >= 0 && index < mStatusArr.Length) {
+				return mStatusArr [index];
+			} else {
+				return false;
+			}
+		}
+
+		public void NotifyStatusOff (int index)
+		{
+			if (index >= 0 && index < mStatusArr.Length) {
+				if (mStatusArr [index] != false) {
+					mStatusArr [index] = false;
+					if (canNotify ((GameUINotifyType)index)) {
+						changeNotify (index);
+					}
+				}
+			}
+		}
+		
+		public void NotifyStatus (int index, bool status)
+		{
+			if (index >= 0 && index < mStatusArr.Length) {
+				if (mStatusArr [index] != status) {
+					mStatusArr [index] = status;
+					if (canNotify ((GameUINotifyType)index)) {
+						changeNotify (index);
+					}
+				}
+			}
+		}
+		
+		
+		public void RegistListener (int index, System.Action onChange)
+		{
+			if (mChangeListeners.ContainsKey (index)) {
+				mChangeListeners [index].Add (onChange);
+			} else {
+				List<System.Action> actions = new List<System.Action> ();
+				actions.Add (onChange);
+				mChangeListeners.Add (index, actions);
+			}
+		}
+		
+		public void UnRegistListener (int index, System.Action onChange)
+		{
+			if (mChangeListeners.ContainsKey (index)) {
+				List<System.Action> actions = mChangeListeners [index];
+				actions.Remove (onChange);
+				if (actions.Count == 0)
+					mChangeListeners.Remove (index);
+			}
+		}
+		
+		private void changeNotify (int index)
+		{
+			if (mChangeListeners.ContainsKey (index)) {
+				List<System.Action> actions = mChangeListeners [index];
+				foreach (Action a in actions) {
+					try {
+						a ();
+					} catch (Exception) {
+					}
+				}
+			}
+		}
+
+		private void changeNotify (List<int> changes)
+		{
+			HashSet<System.Action> actions = new HashSet<System.Action> ();
+			
+			foreach (int key in changes) {
+				if (mChangeListeners.ContainsKey (key)) {
+					List<Action> list = mChangeListeners [key];
+					foreach (Action a in list) {
+						actions.Add (a);
+					}
+				}
+			}
+			
+			foreach (Action a in actions) {
+				try {
+					a ();
+				} catch (Exception) {
+				}
+			}
+			
+		}
+
+		private bool canNotify (GameUINotifyType type)
+		{
+			bool b = true;
+			return b;
+		}
+	}
+}
---
+++ GameUINotifySystem/GameUINotifyRedDot.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Zz.GameUINotifySystem
+{
+	public class GameUINotifyRedDot : MonoBehaviour
+	{
+		private List<IGameUINotifyEvent> customEvents = new List<IGameUINotifyEvent>();
+		public List<GameUINotifyType> NotifyTypes;
+		public GameObject RedDot;
+		private bool mInited = false;
+
+		// Use this for initialization
+		void Start()
+		{
+			if (!mInited)
+			{
+				mInited = true;
+				foreach (GameUINotifyType type in NotifyTypes)
+				{
+					GameUINotifyManager.Instance.RegistListener((int)type, this.OnChange);
+				}
+			}
+
+			OnChange();
+		}
+
+		public void OnEnable()
+		{
+			OnChange();
+		}
+
+		public void OnChange()
+		{
+			if (!enabled)
+				return;
+
+			bool show = false;
+			foreach (GameUINotifyType type in NotifyTypes)
+			{
+				if (GameUINotifyManager.Instance.IsNeedNotify((int)type))
+				{
+					show = true;
+					break;
+				}
+			}
+			Debug.Log("UINotifyRedDot::OnChange after IsNeedNotify show value.");
+
+			if (!show && customEvents != null)
+			{
+				foreach (var _cusEvent in customEvents)
+				{
+					if (_cusEvent != null && _cusEvent.CheckStatus())
+					{
+						show = true;
+					}
+				}
+			}
+
+			if (RedDot == null)
+				return;
+
+			if (show)
+			{
+				RedDot.SetActive(true);
+			}
+			else
+			{
+				RedDot.SetActive(false);
+			}
+		}
+
+		void OnDestroy()
+		{
+			foreach (GameUINotifyType type in NotifyTypes)
+			{
+				GameUINotifyManager.Instance.UnRegistListener((int)type, this.OnChange);
+			}
+			customEvents.Clear();
+			customEvents = null;
+		}
+
+		public void UnregisterCustomAction()
+		{
+			if (customEvents != null)
+				customEvents.Clear();
+		}
+
+		public void RegisterCustomAction(IGameUINotifyEvent _customEvent)
+		{
+			if (customEvents == null)
+			{
+				customEvents = new List<IGameUINotifyEvent>();
+			}
+			customEvents.Add(_customEvent);
+
+			OnChange();
+		}
+	}
+}
